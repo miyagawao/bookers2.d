ecd129863a5c82ec9fcd4dd6c763d6791393f343
miyagawao <ec2-user@ip-172-31-31-224.ap-northeast-1.compute.internal>
2023-05-22 10:19:00 +0000

    Add タグ検索機能

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
   end 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'search' => 'searches#search'
+  get 'tagsearches/search', to: 'tagsearches#search'
   
   resources :chats, only: [:show, :create]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_22_090452) do
+ActiveRecord::Schema.define(version: 2023_05_22_094412) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 2023_05_22_090452) do
     t.text "body"
     t.integer "user_id"
     t.float "star"
+    t.string "category"
   end
 
   create_table "chats", force: :cascade do |t|
